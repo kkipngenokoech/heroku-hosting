1162982cf68eaf21e32f5998c38cbc8a47a0e76a
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-09-02 19:19:31 +0300

    new updates for heroku deployment

--- herokutesthost/settings.py
+++ herokutesthost/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fv3u#@zn6o^knf1f#!*5o34ynckry45iy+akij12fwy^h(_)y#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
